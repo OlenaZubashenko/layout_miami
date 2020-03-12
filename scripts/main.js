8e5814527279d5ba2ae7a93d03d588f9c5ec1b73
olenazubashenko <olenazubashenko@gmail.com>
2020-03-12 09:46:43 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const toggleNav = () => {
+  const toggleEls = document.getElementsByClassName('toggle');
+
+  for (const elem of toggleEls) {
+    elem.classList.toggle('non-visible');
+  }
+};
+
+// TODO: This is for linters:
+toggleNav();
+toggleNav();
